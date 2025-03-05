152796d2aca51a6928f0ae8fd74011706fa7bca6
akash <aakash.aggarwal03@gmail.com>
2025-03-06 00:25:24 +0530

    Fixed output directory.

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,7 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "build", // 🔥 This will generate the build in "build" folder
+  },
 }));
